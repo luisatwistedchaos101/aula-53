d8e600c304ab319caeb1b7a8e9a87e8b591d8aa2
luisatwistedchaos101 <luisaseafox@gmail.com>
2024-12-06 15:23:25 -0300

    Scrr Deus

--- frontend/js/menuToggle.js
+++ frontend/js/menuToggle.js
@@ -4,3 +4,24 @@ const navLinks = document.querySelector(".nav-links");
 navToggle.addEventListener("click", () => {
   navLinks.classList.toggle("nav-links-active"); // Adiciona ou remove a classe que exibe o menu
 });
+
+function registrar(){
+  document.getElementById("name").value;
+  document.getElementById("email").value;
+};
+
+const ImputName =  document.getElementById("name").value;
+ImputName.addEventListener("submit", function(name){
+  name.preventDefault();
+
+});
+
+const email = "nameexample@gmail.com"
+const regex = /^[A-Za-z]{3,}$/;
+
+if (regex.test(email)){
+  console.log("E-mail válido!");
+
+} else {
+  console.log("E-mail inválido!");
+}
